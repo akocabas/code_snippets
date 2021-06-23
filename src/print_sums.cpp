bc90c2281ea37d2054d519807816b77637312c70
akocabas <86250371+akocabas@users.noreply.github.com>
2021-06-23 15:27:34 +0200

    summing the vector with std::accumulate

--- src/print_sums.cpp
+++ src/print_sums.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <numeric>
 
 using my_map_t = std::map<std::string, std::vector<double>>;
 
 void PrintMapSums(const my_map_t& map)
 {
     // Range based for loop iterating through the pairs of the map
-    for (auto const &pair : map ) {
+    for (auto const &pair : map) {
         // records the sum the elements of each vector
         // starts from zero for each vector
-        double sum_of_elements = 0.0;
-        // access the vector using pair . second
-        for (auto &n : pair.second ) {
-            sum_of_elements += n ;
-        }
+        double sum_of_elements = std::accumulate(pair.second.begin(), pair.second.end(), 0);
+
         std::cout << pair.first << " : " << sum_of_elements << std::endl ;
     }
 }
+
